e3bb748dcc34d3eb4436756dfc6526593c7d61e3
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2021-11-21 20:18:01 +0300

    playing again with utility types

---
+++ src/utility-types.ts
@@ -0,0 +1,43 @@
+type Name = {
+  first: string;
+  last: string;
+};
+
+function addFullName(name: Name): Name & {
+  fullName: string;
+} {
+  return {
+    ...name,
+    fullName: `${name.first} ${name.last}`,
+  };
+}
+
+function permuteRows<T extends (...args: any[]) => any>(
+  iteratorFunc: T,
+  data: Parameters<T>[0][]
+): ReturnType<T>[] {
+  return data.map(iteratorFunc);
+}
+
+console.log(permuteRows(addFullName, [{ first: "Muhammed", last: "Ubeyd" }]));
+
+class PersonWithFullName {
+  constructor(public name: Name) {}
+
+  get fullName() {
+    return `${this.name.first} ${this.name.last}`;
+  }
+}
+
+function createObjects<T extends new (...args: any[]) => any>(
+  ObjectType: T,
+  data: ConstructorParameters<T>
+): InstanceType<T>[] {
+  return data.map(item => new ObjectType(item));
+}
+
+console.log(
+  createObjects(PersonWithFullName, [
+    { first: "Jack", last: "HerringTon" },
+  ]).map(obj => obj.fullName)
+);
